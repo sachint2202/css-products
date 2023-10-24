e4cc0bd3ef9328df366a4bba68a298942ca05dae
sachintiwari <32461337+sachint2202@users.noreply.github.com>
2023-10-24 21:58:34 +0530

    add a feature

--- css-product/src/app/app-routing.module.ts
+++ css-product/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'spinner', loadChildren: () => import('./feature/spinner/spinner.module').then(m => m.SpinnerModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
